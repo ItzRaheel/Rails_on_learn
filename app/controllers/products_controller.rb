539991877ef6e0f67916a5f7c7cf34790013485a
raheelzafar605 <raheelzafar605@gmail.com>
2025-08-01 19:12:14 +0500

    initail commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -59,9 +59,16 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[about edit update destroy]
   before_action :authenticate_user!,only: %i[about edit update destroy]
-  
-  def home
+  load_and_authorize_resource 
 
+rescue_from CanCan::AccessDenied do |exception|
+  flash[:notice]= "Only admin can acess these  pages SHOW ,Delete,update"
+  redirect_to root_path
+end
+
+
+  def home
+    @product = Product.accessible_by(current_ability)
     if current_user&.admin?
       @admin_content = "Wellcome Admin"
     else
